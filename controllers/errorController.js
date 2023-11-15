84b3ce433623000697213d8b3c2ed41dac2bc81b
Shahadat Hossen <shahadat.07.sh@gmail.com>
2023-11-15 17:19:48 +0600

    code updated

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -77,18 +77,28 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
+  let error = { ...err };
 
-    if (error.name === "CastError") error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === "ValidationError")
-      error = handleValidationErrorDB(error);
-    if (error.name === "JsonWebTokenError") error = handleJWTError();
-    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+  if (error.name === "CastError") error = handleCastErrorDB(error);
+  if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+  if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+  if (error.name === "JsonWebTokenError") error = handleJWTError();
+  if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
 
-    sendErrorProd(error, res);
-  }
+  sendErrorProd(error, res);
+
+  // if (process.env.NODE_ENV === "development") {
+  //   sendErrorDev(err, res);
+  // } else if (process.env.NODE_ENV === "production") {
+  //   let error = { ...err };
+
+  //   if (error.name === "CastError") error = handleCastErrorDB(error);
+  //   if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+  //   if (error.name === "ValidationError")
+  //     error = handleValidationErrorDB(error);
+  //   if (error.name === "JsonWebTokenError") error = handleJWTError();
+  //   if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+
+  //   sendErrorProd(error, res);
+  // }
 };
