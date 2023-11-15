4cc6df42654bbfeb4562e16d5329b0b37805d3c5
Shahadat Hossen <shahadat.07.sh@gmail.com>
2023-11-15 18:13:55 +0600

    api updated

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -73,20 +73,38 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
+  let error = { ...err };
 
-    // console.log("error", error, error.name);
+  // console.log("error", error, error.name);
 
-    if (error.name === "CastError") error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === "ValidationError")
-      error = handleValidationErrorDB(error);
-    if (error.name === "JsonWebTokenError") error = handleJWTError();
-    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+  if (error.name === "CastError") error = handleCastErrorDB(error);
+  if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+  if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+  if (error.name === "JsonWebTokenError") error = handleJWTError();
+  if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
 
-    sendErrorProd(error, res);
-  }
+  sendErrorProd(error, res);
 };
+// module.exports = (err, req, res, next) => {
+//   // console.log(err.stack);
+
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || "error";
+
+//   if (process.env.NODE_ENV === "development") {
+//     sendErrorDev(err, res);
+//   } else if (process.env.NODE_ENV === "production") {
+//     let error = { ...err };
+
+//     // console.log("error", error, error.name);
+
+//     if (error.name === "CastError") error = handleCastErrorDB(error);
+//     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+//     if (error.name === "ValidationError")
+//       error = handleValidationErrorDB(error);
+//     if (error.name === "JsonWebTokenError") error = handleJWTError();
+//     if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+
+//     sendErrorProd(error, res);
+//   }
+// };
